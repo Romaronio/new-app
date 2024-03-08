e5693cfae5c450f1c0e69132ba6814faf6ba9e57
Romaronio <144565657+Romaronio@users.noreply.github.com>
2024-03-08 22:10:07 +0300

    создал новую ветку

---
+++ src/coponents/Dialogs/Dialogs.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import s from './Dialogs.module.css'
+let Dialogs = props => {
+	let MasChat = props.chat.map(D => (
+		<NavLink to={D.id}>
+			<button>{D.name}</button>
+		</NavLink>
+	))
+	return (
+		<div>
+			{/* <div>{MasChat}</div> */}
+			<textarea
+				name=''
+				id=''
+				className={s.Dialogs}
+				cols='100'
+				rows='3'
+			></textarea>
+			<button className={s.button}>Отправит домой спать</button>
+		</div>
+	)
+}
+export default Dialogs
